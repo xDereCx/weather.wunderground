e6d15839108de72236ef33069d489251f6a86d6f
minipretzel <minipretzel@users.noreply.github.com>
2014-10-15 20:35:39 -0400

    Add your personal api key
    Grab an apikey from weatherunderground.com.  Free api allows 500 hits
    per day.

--- resources/lib/wunderground.py
+++ resources/lib/wunderground.py
@@ -3,11 +3,13 @@
 import urllib2, gzip, base64
 from StringIO import StringIO
 
+APIKEY           = 'PUT_YOUR_PERSONAL_API_KEY_HERE'
 WAIK             = ''
 WUNDERGROUND_URL = 'http://api.wunderground.com/api/%s/%s/%s/q/%s.%s'
 
 def wundergroundapi(features, settings, query, fmt):
-    url = WUNDERGROUND_URL % (base64.b64decode(WAIK)[::-1], features, settings, query, fmt)
+    #url = WUNDERGROUND_URL % (base64.b64decode(WAIK)[::-1], features, settings, query, fmt)
+    url = WUNDERGROUND_URL % (APIKEY, features, settings, query, fmt)
     try:
         req = urllib2.Request(url)
         req.add_header('Accept-encoding', 'gzip')
